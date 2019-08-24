59aa6736b5f526020d0c60da2c022a7df04c122c
Andrija <aglavas11@gmail.com>
2019-08-24 14:00:29 +0200

    Visual changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,34 +25,37 @@
 			<header id="m_header" class="m-grid__item m-header" m-minimize-offset="200" m-minimize-mobile-offset="200">
 				<div class="m-container m-container--fluid m-container--full-height">
 					<div class="m-stack m-stack--ver m-stack--desktop">
+
+                        {{--sidebar--}}
+
                         @if(Route::current()->getName() != 'get.answer')
-						<div class="m-stack__item m-brand  m-brand--skin-dark">
-							<div class="m-stack m-stack--ver m-stack--general">
-								<div class="m-stack__item m-stack__item--middle m-brand__logo">
-									{{--<a href="{{ route('home') }}" class="m-brand__logo-wrapper">--}}
-										{{--<img data-src="{{ asset('img/logo.png') }}" alt="Logo" width="150" class="lazy-load">--}}
-									{{--</a>--}}
-								</div>
-                                    <div class="m-stack__item m-stack__item--middle m-brand__tools">
-                                        <!-- BEGIN: Left Aside Minimize Toggle -->
-                                        <a href="#" id="m_aside_left_minimize_toggle" class="m-brand__icon m-brand__toggler m-brand__toggler--left m--visible-desktop-inline-block hide
-                         ">
-                                            <span></span>
-                                        </a>
-                                        <!-- END -->
-                                        <!-- BEGIN: Responsive Aside Left Menu Toggler -->
-                                        <a href="#" id="m_aside_left_offcanvas_toggle" class="m-brand__icon m-brand__toggler m-brand__toggler--left m--visible-tablet-and-mobile-inline-block">
-                                            <span></span>
-                                        </a>
-                                        <!-- END -->
-                                        <!-- BEGIN: Topbar Toggler -->
-                                        <a id="m_aside_header_topbar_mobile_toggle" href="#" class="m-brand__icon m--visible-tablet-and-mobile-inline-block">
-                                            <i class="flaticon-more"></i>
-                                        </a>
-                                        <!-- BEGIN: Topbar Toggler -->
+                            <div class="m-stack__item m-brand  m-brand--skin-dark">
+                                <div class="m-stack m-stack--ver m-stack--general">
+                                    <div class="m-stack__item m-stack__item--middle m-brand__logo">
+                                        {{--<a href="{{ route('home') }}" class="m-brand__logo-wrapper">--}}
+                                            {{--<img data-src="{{ asset('img/logo.png') }}" alt="Logo" width="150" class="lazy-load">--}}
+                                        {{--</a>--}}
                                     </div>
-							</div>
-						</div>
+                                        <div class="m-stack__item m-stack__item--middle m-brand__tools">
+                                            <!-- BEGIN: Left Aside Minimize Toggle -->
+                                            <a href="#" id="m_aside_left_minimize_toggle" class="m-brand__icon m-brand__toggler m-brand__toggler--left m--visible-desktop-inline-block hide
+                             ">
+                                                <span></span>
+                                            </a>
+                                            <!-- END -->
+                                            <!-- BEGIN: Responsive Aside Left Menu Toggler -->
+                                            <a href="#" id="m_aside_left_offcanvas_toggle" class="m-brand__icon m-brand__toggler m-brand__toggler--left m--visible-tablet-and-mobile-inline-block">
+                                                <span></span>
+                                            </a>
+                                            <!-- END -->
+                                            <!-- BEGIN: Topbar Toggler -->
+                                            <a id="m_aside_header_topbar_mobile_toggle" href="#" class="m-brand__icon m--visible-tablet-and-mobile-inline-block">
+                                                <i class="flaticon-more"></i>
+                                            </a>
+                                            <!-- BEGIN: Topbar Toggler -->
+                                        </div>
+                                </div>
+                            </div>
                         @endif
 
                         @if(Route::current()->getName() != 'get.answer')
@@ -62,65 +65,10 @@
                                 </div>
                             </nav>
                         @endif
-
-                        {{--<div class="row">--}}
-                            {{--<div class="col-lg-6">--}}
-                                {{--{{ Breadcrumbs::render('dashboard') }}--}}
-                            {{--</div>--}}
-                            {{--<div class="col-lg-6">--}}
-                                {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-                                    {{--<ul class="navbar-nav mr-auto">--}}
-                                        {{--<li class="nav-item dropdown">--}}
-                                            {{--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-                                                {{--Dropdown--}}
-                                            {{--</a>--}}
-                                            {{--<div class="dropdown-menu" aria-labelledby="navbarDropdown">--}}
-                                                {{--<a class="dropdown-item" href="#">Action</a>--}}
-                                                {{--<a class="dropdown-item" href="#">Another action</a>--}}
-                                                {{--<div class="dropdown-divider"></div>--}}
-                                                {{--<a class="dropdown-item" href="#">Something else here</a>--}}
-                                            {{--</div>--}}
-                                        {{--</li>--}}
-                                    {{--</ul>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
 						<div class="m-stack__item m-stack__item--fluid m-header-head" id="m_header_nav">
 							<div id="m_header_topbar" class="m-topbar m-stack m-stack--ver m-stack--general m-stack--fluid">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-6">
-                                        {{--<ul class="nav navbar-nav">--}}
-                                            {{--<li aria-haspopup="true" class="menu-dropdown classic-menu-dropdown active">--}}
-                                                {{--<a href="javascript:;"> Layouts--}}
-                                                    {{--<span class="arrow"></span>--}}
-                                                {{--</a>--}}
-                                                {{--<ul class="dropdown-menu pull-left">--}}
-                                                    {{--<li aria-haspopup="true" class=" ">--}}
-                                                        {{--<a href="layout_mega_menu_light.html" class="nav-link  "> Light Mega Menu </a>--}}
-                                                    {{--</li>--}}
-                                                    {{--<li aria-haspopup="true" class=" active">--}}
-                                                        {{--<a href="layout_top_bar_light.html" class="nav-link  active"> Light Top Bar Dropdowns </a>--}}
-                                                    {{--</li>--}}
-                                                    {{--<li aria-haspopup="true" class=" ">--}}
-                                                        {{--<a href="layout_fluid_page.html" class="nav-link  "> Fluid Page </a>--}}
-                                                    {{--</li>--}}
-                                                    {{--<li aria-haspopup="true" class=" ">--}}
-                                                        {{--<a href="layout_top_bar_fixed.html" class="nav-link  "> Fixed Top Bar </a>--}}
-                                                    {{--</li>--}}
-                                                    {{--<li aria-haspopup="true" class=" ">--}}
-                                                        {{--<a href="layout_mega_menu_fixed.html" class="nav-link  "> Fixed Mega Menu </a>--}}
-                                                    {{--</li>--}}
-                                                    {{--<li aria-haspopup="true" class=" ">--}}
-                                                        {{--<a href="layout_disabled_menu.html" class="nav-link  "> Disabled Menu Links </a>--}}
-                                                    {{--</li>--}}
-                                                    {{--<li aria-haspopup="true" class=" ">--}}
-                                                        {{--<a href="layout_blank_page.html" class="nav-link  "> Blank Page </a>--}}
-                                                    {{--</li>--}}
-                                                {{--</ul>--}}
-                                            {{--</li>--}}
-                                        {{--</ul>--}}
-
                                         @if(Route::current()->getName() == 'dashboard')
                                             <nav class="navbar navbar-expand navbar-light bg-light">
                                                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -307,16 +255,6 @@
                 });
 
             return false; } );
-
-            // $('#changeMode').button().click(function(){
-            //     $('#playfield').toggle(function() {
-            //         $(this).toggleClass("gridView plainView");
-            //         //$(this).removeClass("gridView").addClass("plainView");
-            //     }, function() {
-            //         $(this).toggleClass("plainView gridView");
-            //         //$(this).removeClass("plainView").addClass("gridView");
-            //     });
-            // });
         });
 </script>
 </html>
--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -1,28 +1,13 @@
 @if (count($breadcrumbs))
-
-    {{--<div style="display: inline">--}}
-    {{--@foreach ($breadcrumbs as $breadcrumb)--}}
-
-        {{--@if ($breadcrumb->url && !$loop->last)--}}
-            {{--<a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a> >--}}
-        {{--@else--}}
-            {{--{{ $breadcrumb->title }}--}}
-        {{--@endif--}}
-
-    {{--@endforeach--}}
-    {{--</div>--}}
-
     <ol class="breadcrumb">
         @foreach ($breadcrumbs as $breadcrumb)
-
             @if ($breadcrumb->url && !$loop->last)
-                <li><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
-                {{--<li class="breadcrumb-item breadcrumb-list"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>--}}
+                <li class="breadcrumb-item breadcrumb-list"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title}} &nbsp;</a></li>
+            @elseif ($breadcrumb->url && $loop->first)
+                <li> {{$breadcrumb->title }}</li>
             @else
-                <li>{{ $breadcrumb->title }}</li>
+                <li> {{' > ' . $breadcrumb->title }}</li>
             @endif
-
         @endforeach
     </ol>
-
 @endif
